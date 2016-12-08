9ef937cd69108198ba9684200a615ef313205455
Ihor <moskigo2@gmail.com>
2016-12-08 18:10:12 +0200

    added UI WFApp

---
+++ WinFormUniversitySort/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormUniversitySort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveChanges_button_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Are you sure?", "Confirm", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                
+            }
+        }
+    }
+}
