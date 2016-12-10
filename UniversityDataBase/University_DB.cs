8d56657748f006f26b2ff9db369c834ac3f2509c
Ihor <moskigo2@gmail.com>
2016-12-10 11:53:11 +0200

    pushOnBranch

---
+++ UniversityDataBase/University_DB.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+
+namespace University_DB
+{
+    public class UniversityDB : DbContext
+    {
+        public UniversityDB()
+            : base("ModelDb")
+        { }
+
+        public DbSet<Software> Software { get; set; }
+        public DbSet<SoftwarePackage> SoftwarePackage { get; set; }
+        public DbSet<Audience> Audience { get; set; }
+        public DbSet<Semester> Semester { get; set; }
+        public DbSet<Group> Group { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
